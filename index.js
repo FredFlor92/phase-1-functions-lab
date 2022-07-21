b43ad07f68183c8aacd7cd7244d8b4dec1d41289
Freddy Florial <freddyflorial@gmail.com>
2022-07-21 06:11:23 -0400

    commit tree af40ea520e34581fd3ece2d19db740936a631f6c

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+let distance, someValue;
+const headquarters = 42;
+function distanceFromHqInBlocks(someValue){
+    if (someValue >= headquarters){
+        distance =someValue - 42
+    }else if (someValue < headquarters){
+        distance = 42 - someValue
+    }
+    return distance
+}
+
+function distanceFromHqInFeet(someValue){
+    distanceFromHqInBlocks(someValue)
+}
+
+function distanceTravelledInFeet(start, destination){
+
+}
+
+function calculatesFarePrice(start, destination){
+
+}
